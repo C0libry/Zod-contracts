cc295b79d40b2f96cee903c84732640152dddafb
Max <pe.pe.l@yandex.ru>
2024-09-04 12:35:21 +0300

    Update FindUserResponseSchema path to absolut

--- bybit-traiding-bot/create-user.ts
+++ bybit-traiding-bot/create-user.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
-import { FindUserResponseSchema } from 'contracts/auth';
+import { FindUserResponseSchema } from '../auth';
 
 export const CreateUserRequestSchema = FindUserResponseSchema.omit({
   createdAt: true,
