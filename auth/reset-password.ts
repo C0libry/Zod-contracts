314312d0be6fb32378035a300fea7014a19cfc2d
Max <pe.pe.l@yandex.ru>
2024-09-04 20:21:18 +0300

    Update reset-password.ts

--- auth/reset-password.ts
+++ auth/reset-password.ts
@@ -1,6 +1,15 @@
 import { z } from 'zod';
 import { PasswordSchema } from './password';
-export const ResetPasswordRequestSchema = PasswordSchema.extend({});
+export const ResetPasswordRequestSchema = PasswordSchema.extend({
+  resetPasswordToken: z
+    .string()
+    .regex(/^[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/, {
+      message: 'Invalid token',
+    }),
+}).refine((data) => data.password === data.confirmPassword, {
+  message: `Passwords not match`,
+  path: ['confirmPassword'],
+});
 
 export type ResetPasswordRequest = z.infer<typeof ResetPasswordRequestSchema>;
 export type ResetPasswordResponse = true;
