94f3bfd10875189cac1f686440722adb1aac9d47
Max <pe.pe.l@yandex.ru>
2024-09-07 11:22:25 +0300

    Added userId to CreateApiKeyRequestSchema

--- bybit-traiding-bot/create-api-key.ts
+++ bybit-traiding-bot/create-api-key.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
 
 export const CreateApiKeyRequestSchema = z.object({
+  userId: z.string().uuid(),
   apiKeyName: z.string().min(1, 'Required'),
   apiKey: z.string().min(1, 'Required'),
   apiSecret: z.string().min(1, 'Required'),
