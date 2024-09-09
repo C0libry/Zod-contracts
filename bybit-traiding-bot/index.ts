670e240f6fa68b57867f2440ea2495ecc3d1c19e
Max <pe.pe.l@yandex.ru>
2024-09-09 23:48:52 +0300

    Moved orders-grid.ts to user folder

--- bybit-traiding-bot/index.ts
+++ bybit-traiding-bot/index.ts
@@ -1,4 +1,4 @@
 export * from './bybit-api-key/create-api-key';
 export * from './bybit-api-key/update-api-key';
 export * from './user/create-user';
-export * from './orders-grid';
+export * from './grid-bot/orders-grid';
