d791f0a39b01a1e6726d70d28e5e948eb42c3c8f
Max <pe.pe.l@yandex.ru>
2024-09-10 00:15:37 +0300

    Created set-default-api-key.ts

---
+++ bybit-traiding-bot/user/set-default-api-key.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+export const SetDefaultApiKeyRequestSchema = z.object({
+  apiKeyId: z.string().uuid(),
+});
+
+export const SetDefaultApiKeyWithUserIdRequestSchema =
+  SetDefaultApiKeyRequestSchema.extend({ id: z.string().uuid() });
+
+export type SetDefaultApiKeyRequest = z.infer<
+  typeof SetDefaultApiKeyRequestSchema
+>;
+
+export type SetDefaultApiKeyWithUserIdRequest = z.infer<
+  typeof SetDefaultApiKeyWithUserIdRequestSchema
+>;
