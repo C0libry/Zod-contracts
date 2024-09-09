7eea21f2238f80775a1405432f966834b96ba203
Max <pe.pe.l@yandex.ru>
2024-09-10 00:16:20 +0300

    Update index.ts

--- bybit-traiding-bot/index.ts
+++ bybit-traiding-bot/index.ts
@@ -1,5 +1,6 @@
 export * from './bybit-api-key/create-api-key';
 export * from './bybit-api-key/remove-api-key';
 export * from './bybit-api-key/update-api-key';
-export * from './user/create-user';
 export * from './grid-bot/orders-grid';
+export * from './user/create-user';
+export * from './user/set-default-api-key';
