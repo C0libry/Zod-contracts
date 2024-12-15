9cb372a4b0b14126dbf53c1dbfe4a83b01f82705
Max <pe.pe.l@yandex.ru>
2024-12-15 20:09:05 +0300

    Update create-api-key, remove-api-key, orders-grid. Add request with userId

--- bybit-traiding-bot/bybit-api-key/create-api-key.ts
+++ bybit-traiding-bot/bybit-api-key/create-api-key.ts
@@ -1,10 +1,18 @@
 import { z } from 'zod';
 
 export const CreateApiKeyRequestSchema = z.object({
-  userId: z.string().uuid().optional(),
   apiKeyName: z.string().min(1, 'Required'),
   apiKey: z.string().min(1, 'Required'),
   apiSecret: z.string().min(1, 'Required'),
 });
 
+export const CreateApiKeyWithUserIdRequestSchema =
+  CreateApiKeyRequestSchema.extend({
+    userId: z.string().uuid(),
+  });
+
 export type CreateApiKeyRequest = z.infer<typeof CreateApiKeyRequestSchema>;
+
+export type CreateApiKeyWithUserIdRequest = z.infer<
+  typeof CreateApiKeyWithUserIdRequestSchema
+>;
--- bybit-traiding-bot/bybit-api-key/remove-api-key.ts
+++ bybit-traiding-bot/bybit-api-key/remove-api-key.ts
@@ -1,8 +1,14 @@
 import { z } from 'zod';
 
 export const RemoveApiKeyRequestSchema = z.object({
-  userId: z.string().uuid().optional(),
   apiKeyName: z.string().min(1, 'Required'),
 });
 
+export const RemoveApiKeyWithUserIdRequestSchema =
+  RemoveApiKeyRequestSchema.extend({ userId: z.string().uuid() });
+
 export type RemoveApiKeyRequest = z.infer<typeof RemoveApiKeyRequestSchema>;
+
+export type RemoveApiKeyWithUserIdRequest = z.infer<
+  typeof RemoveApiKeyWithUserIdRequestSchema
+>;
