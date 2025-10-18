89c775ef65028ac9b981654e132080d1f745882e
Maxim Blokhin <pe.pe.l@yandex.ru>
2025-10-18 11:51:11 +0300

    Move set-default-api-key to bybit-api-key

--- bybit-traiding-bot/index.ts
+++ bybit-traiding-bot/index.ts
@@ -3,4 +3,4 @@ export * from './bybit-api-key/remove-api-key';
 export * from './bybit-api-key/update-api-key';
 export * from './grid-bot/orders-grid';
 export * from './user/create-user';
-export * from './user/set-default-api-key';
+export * from './bybit-api-key/set-default-api-key';
